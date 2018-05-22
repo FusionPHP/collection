671fab9e3db6673834e69d4a0f9349cfde8fe783
Jason L. Walker <jasonlanewalker@gmail.com>
2018-05-21 21:00:08 -0500

    created new test for collection validation

---
+++ tests/CollectionValidatorTest.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Part of the Fusion.Collection package test suite.
+ *
+ * @license MIT
+ */
+
+namespace Fusion\Collection\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class CollectionValidatorTest extends TestCase
+{
+
+}
